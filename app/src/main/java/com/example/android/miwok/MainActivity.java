4637bcd0d4941fe5f29bcba8fed10cb9724ef4dd
Shaun Alvarez <Shaun Alvarez>
2017-03-05 19:27:27 -0800

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwok;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.android.miwok.ColorsActivity;
+import com.example.android.miwok.FamilyActivity;
+import com.example.android.miwok.NumbersActivity;
+import com.example.android.miwok.PhrasesActivity;
+import com.example.android.miwok.R;
+
+public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+            TextView numbers = (TextView)findViewById(R.id.numbers);
+            TextView family = (TextView)findViewById(R.id.family);
+            TextView colors = (TextView)findViewById(R.id.colors);
+            TextView phrases = (TextView)findViewById(R.id.phrases);
+
+            numbers.setOnClickListener(new View.OnClickListener()
+             {
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this,
+                        NumbersActivity.class);
+
+                startActivity(numbersIntent);
+                }
+            });
+
+            family.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view) {
+                    Intent familyIntent = new Intent(MainActivity.this,
+                            FamilyActivity.class);
+
+                    startActivity(familyIntent);
+                }
+            });
+
+            colors.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view) {
+                    Intent colorsIntent = new Intent(MainActivity.this,
+                            ColorsActivity.class);
+
+                    startActivity(colorsIntent);
+                }
+            });
+
+            phrases.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view) {
+                    Intent phrasesIntent = new Intent(MainActivity.this,
+                            PhrasesActivity.class);
+
+                    startActivity(phrasesIntent);
+                }
+            });
+    }
+}
